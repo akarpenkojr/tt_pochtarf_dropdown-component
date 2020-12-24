8dd5a7650665b93b66de7c55f34444417b87f49a
akarpenko_jr <andy.karpenko737@gmail.com>
2020-12-24 22:50:23 +0300

    Added component for option items

---
+++ src/components/OptionItems/OptionItems.js
@@ -0,0 +1,26 @@
+//imports
+import React, {Component} from 'react'
+
+// import components
+
+// import styles
+
+class OptionItems extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		};
+
+	}
+
+   render() {
+      return (
+         <div>
+
+         </div>
+      )
+   }
+}
+
+export default OptionItems
