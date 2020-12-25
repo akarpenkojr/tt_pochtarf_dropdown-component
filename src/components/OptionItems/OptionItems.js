0093798cee40c8cc8f13d89765f56417c991984f
akarpenko_jr <andy.karpenko737@gmail.com>
2020-12-25 12:49:59 +0300

    Added style highlighting to selected items

--- src/components/OptionItems/OptionItems.js
+++ src/components/OptionItems/OptionItems.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 
 // import components
 import styles from './OptionItems.module.scss'
+import checkboxArrow from './checkboxArrowIcon.svg'
 
 // import styles
 
@@ -15,18 +16,36 @@ class OptionItems extends Component {
 
 	}
 
+	selectItem = (id) => {
+		let selectedOptionItemIndex = this.props.selectedOptionItems.map(item => {
+			return item[0].id;
+		}).indexOf(id);
+		return selectedOptionItemIndex !== -1 ? ({
+				backgroundColor: 'rgba(245, 246, 250, 1)'
+		}) : null;
+	};
+
+	selectCheckbox = (id) => {
+		let selectedOptionItemIndex = this.props.selectedOptionItems.map(item => {
+			return item[0].id;
+		}).indexOf(id);
+		return selectedOptionItemIndex !== -1 ? ({
+			borderColor: 'transparent',
+			backgroundColor: 'rgba(73, 92, 100, 1)',
+			backgroundImage: `url(${checkboxArrow})`,
+		}) : null;
+	};
    render() {
-		console.log('___---_-_!!!_-_---___', this.props);
       return (
-         <div className={styles.optionsAreaWrapper} >
+         <div className={styles.optionsAreaWrapper}>
 					 {this.state.items.map(item => {
 					 		return (
 					 				<div
 											key={item.id}
 											onClick={() => this.props.selectOption(this.props.items, item.id)}
 									>
-										<div className={styles.optionItem}>
-											<div className={styles.optionItem__checkbox}>
+										<div className={styles.optionItem} style={this.selectItem(item.id)}>
+											<div className={styles.optionItem__checkbox} style={this.selectCheckbox(item.id)}>
 
 											</div>
 											<div className={styles.optionItem__data}>
